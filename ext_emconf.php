b446f75fbc19d86c2ed98781513a8384560b4c0e
Daniel Siepmann <coding@daniel-siepmann.de>
2018-03-13 08:38:29 +0100

    TASK: Init project files

---
+++ ext_emconf.php
@@ -0,0 +1,25 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Search Core: Twitter',
+    'description' => 'Search core implementation for Twitter.',
+    'category' => 'be',
+    'clearCacheOnLoad' => 1,
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-8.7.99',
+            'php' => '7.0.0-7.2.99',
+            'search_core' => '',
+        ],
+        'conflicts' => [],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Codappix\\SearchCoreTwitter\\' => 'Classes',
+        ],
+    ],
+    'state' => 'beta',
+    'version' => '1.0.0',
+    'author' => 'Daniel Siepmann',
+    'author_email' => 'coding@daniel-siepmann.de',
+];
